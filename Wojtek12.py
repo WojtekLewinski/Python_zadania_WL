efc017c0591beccd66fdfbf8f0704a2c6d45fe65
WojtekLewiński <lewinski.wojciech.dariusz@gmail.com>
2022-04-04 00:20:16 +0200

    Zadanie 12

--- Wojtek12.py
+++ Wojtek12.py
@@ -9,16 +9,24 @@ maksimum
 NIE używaj funkcji wbudowanych!
 """
 
-
-
+licznik = 0
 a = []
-b = int(input('podaj liczbę'))
-c = input('Czy chcesz podać kolejną liczbę T/N ?')
-a.append(b)
-print(f'podane przez ciebie liczby to: {a}')
-if c == 'T':
-    next(b)
-
-
-
-
+while True:
+    licznik +=1
+    c = int(input('podaj liczbę'))
+    d = input('Czy podasz kolejną liczbę T/N')
+    a.append(c)
+    if licznik == 1000000 or d =='N':
+        break
+print(f'Liczba podanych przeż użytkownika liczb to: {len(a)}')
+print(f'Suma podanych przeż użytkownika liczb to: {sum(a)}')
+print(f'Średnia podanych przeż użytkownika liczb to: {sum(a)/len(a)}')
+najmniejsza = None
+najwieksza = None
+for i in a:
+    if najmniejsza == None or najmniejsza > i:
+        najmniejsza = i
+    if najwieksza == None or najwieksza < i:
+        najwieksza = i
+print(f'Najniższa z podanych przeż użytkownika liczb to: {najmniejsza}')
+print(f'Najwyższa z podanych przeż użytkownika liczb to: {najwieksza}')
