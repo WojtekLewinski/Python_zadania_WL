5912b795dc6d1b3c23b7272a3f23604893ce4fed
WojtekLewiński <lewinski.wojciech.dariusz@gmail.com>
2022-04-03 22:33:02 +0200

    Zadanie 11

--- Wojtek11.py
+++ Wojtek11.py
@@ -7,7 +7,7 @@ liniach "choinkę" ze znaków * . Np. dla parametru 3 powinno się wypisać:
 poziomy = int(input(f'Podaj dodatnią liczbę całkowitą:'))
 pierwszy = 1
 for a in range(0,poziomy):
-    print(poziomy* " " + "*" * pierwszy)
+    print(" " * poziomy + "*" * pierwszy)
     poziomy=poziomy-1
     pierwszy = pierwszy + 2
 
