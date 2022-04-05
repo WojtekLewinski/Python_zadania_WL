39e8e97fbc0f96f17faeca63e8156ec5fb3f8cbb
WojtekLewiński <lewinski.wojciech.dariusz@gmail.com>
2022-04-05 22:35:44 +0200

    Zadanie 3

--- Wojtek3.py
+++ Wojtek3.py
@@ -6,22 +6,22 @@ wzór, interpretację wyników, proszę znaleźć samodzielnie)."""
 
 wzrost = int(input("Podaj wzrost w cm"))
 masa_ciala = float(input("Podaj masę ciała w kg"))
-BMI = round((masa_ciala / (wzrost/100)**2),2)
-if BMI <16:
-    print(f'BMI: {float(BMI)} Wygłodzenie (niedowaga)')
-if BMI >= 16 and BMI < 17:
-     print(f'BMI: {float(BMI)} Wychudzenie (niedowaga)')
-if BMI >= 17 and BMI < 18.5:
-    print(f'BMI: {float(BMI)} Niedowaga')
-if BMI >= 18.5 and BMI < 25:
-     print(f'BMI: {float(BMI)} Pożądana masa ciała (optimum)')
-if BMI >= 25 and BMI < 30:
-     print(f'BMI: {float(BMI)} Nadwaga (nadwaga)')
-if BMI >= 30 and BMI < 35:
-     print(f'BMI: {float(BMI)} Otyłość 1 stopnia (otyłość)')
-if BMI >= 35 and BMI < 40:
-     print(f'BMI: {float(BMI)} Otyłość 2 stopnia  (duża otyłość)')
-if BMI >= 40:
-     print(f'BMI: {float(BMI)} Otyłość 3 stopnia (chorobliwa otyłość)')
+bmi = round((masa_ciala / (wzrost/100)**2),2)
+if bmi <16:
+    print(f'BMI: {float(bmi)} Wygłodzenie (niedowaga)')
+if bmi >= 16 and bmi < 17:
+     print(f'BMI: {float(bmi)} Wychudzenie (niedowaga)')
+if bmi >= 17 and bmi < 18.5:
+    print(f'BMI: {float(bmi)} Niedowaga')
+if bmi >= 18.5 and bmi < 25:
+     print(f'BMI: {float(bmi)} Pożądana masa ciała (optimum)')
+if bmi >= 25 and bmi < 30:
+     print(f'BMI: {float(bmi)} Nadwaga (nadwaga)')
+if bmi >= 30 and bmi < 35:
+     print(f'BMI: {float(bmi)} Otyłość 1 stopnia (otyłość)')
+if bmi >= 35 and bmi < 40:
+     print(f'BMI: {float(bmi)} Otyłość 2 stopnia  (duża otyłość)')
+if bmi >= 40:
+     print(f'BMI: {float(bmi)} Otyłość 3 stopnia (chorobliwa otyłość)')
 
 
