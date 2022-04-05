e6a4fd90a393205294bb9cf726a3fd5b24e3b6ca
WojtekLewiński <lewinski.wojciech.dariusz@gmail.com>
2022-04-05 22:39:38 +0200

    Zadanie 3 - poprawione

--- Wojtek3.py
+++ Wojtek3.py
@@ -8,20 +8,20 @@ wzrost = int(input("Podaj wzrost w cm"))
 masa_ciala = float(input("Podaj masę ciała w kg"))
 bmi = round((masa_ciala / (wzrost/100)**2),2)
 if bmi <16:
-    print(f'BMI: {float(bmi)} Wygłodzenie (niedowaga)')
+    print(f'BMI: {bmi} Wygłodzenie (niedowaga)')
 if bmi >= 16 and bmi < 17:
-     print(f'BMI: {float(bmi)} Wychudzenie (niedowaga)')
+     print(f'BMI: {bmi} Wychudzenie (niedowaga)')
 if bmi >= 17 and bmi < 18.5:
-    print(f'BMI: {float(bmi)} Niedowaga')
+    print(f'BMI: {bmi} Niedowaga')
 if bmi >= 18.5 and bmi < 25:
-     print(f'BMI: {float(bmi)} Pożądana masa ciała (optimum)')
+     print(f'BMI: {bmi} Pożądana masa ciała (optimum)')
 if bmi >= 25 and bmi < 30:
-     print(f'BMI: {float(bmi)} Nadwaga (nadwaga)')
+     print(f'BMI: {bmi} Nadwaga (nadwaga)')
 if bmi >= 30 and bmi < 35:
-     print(f'BMI: {float(bmi)} Otyłość 1 stopnia (otyłość)')
+     print(f'BMI: {bmi} Otyłość 1 stopnia (otyłość)')
 if bmi >= 35 and bmi < 40:
-     print(f'BMI: {float(bmi)} Otyłość 2 stopnia  (duża otyłość)')
+     print(f'BMI: {bmi} Otyłość 2 stopnia  (duża otyłość)')
 if bmi >= 40:
-     print(f'BMI: {float(bmi)} Otyłość 3 stopnia (chorobliwa otyłość)')
+     print(f'BMI: {bmi} Otyłość 3 stopnia (chorobliwa otyłość)')
 
 
