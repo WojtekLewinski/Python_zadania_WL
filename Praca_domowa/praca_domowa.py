9e77d0f83d6e1ce9179c9d30531ebeea77b19cf3
WojtekLewiński <lewinski.wojciech.dariusz@gmail.com>
2022-04-20 21:23:23 +0200

    praca_domowa

---
+++ Praca_domowa/praca_domowa.py
@@ -0,0 +1,71 @@
+print("_"*65, "Zadanie 1")
+"""Zadanie 3.1 Funkcje liczbowe
+Stwórz następujące funkcje. Niech każda z nich przyjmuje w argument do przeliczenia i
+zwraca przeliczoną wartość.
+1. stopy_na_metry  - przelicza odległość wyrażoną w stopach na metry,
+2. max  - zwraca większą z dwóch liczb - postaraj się nie używać funkcji max
+wbudowanej w pythona
+3. srednia  - oblicza średnią z dwóch liczb,
+4. pole_kola  - oblicza pole koła o podanym promieniu (jeden parametr) podpowiedź:
+wartość PI jest dostępna jako Math.PI
+5. bmi  - oblicza współczynnik BMI dla podanych dwóch parametrów: wzrostu w
+metrach i wagi w kg.
+6. pole_trojkata  - z trzema parametrami - oblicza pole trójkąta o podanych bokach
+z wzoru Herona.
+7. kilometry_na_mile  - przelicza odległość wyrażoną w kilometrach na mile
+8. mile_na_kilometry  - przelicza odległość wyrażoną w milach na kilometry
+Dla wybranych napisz też interaktywne programy, które pytają użytkownika o dane i wypisują
+wynik."""
+
+def przelicz(stopa:float):
+    dlugosc_a = round(stopa/3.28084,5)
+    print(f'{stopa} ft. to {dlugosc_a} metra')
+przelicz(5)
+
+
+
+
+print("_"*65, "Zadanie 2")
+
+"""Zadanie 3.2 | MiesiąceZapytaj użytkownika o nazwę miesiąca i na tej podstawie wypisz mu ile dni na dany miesiąc.
+Logikę obliczania liczby dni wydziel do osobnej funkcji.
+Wersja A Nie przyjmuj się lutym - zwracaj zawsze jedną wartość.
+Wersja B (trudniejsza) Jeżeli użytkownik poda luty - zapytaj go o rok. Na tej podstawie
+Nauka programowania w Pythonie -
+ćwiczenia domowe
+3. Funkcje
+Zadanie 3.1 Funkcje liczbowe"""
+
+
+print("_"*65, "Zadanie 3")
+"""Zadanie 3.3 | Operacje na listach
+Stwórz następujące funkcje. Każda z nich będzie przyjmowała listę liczb i na tej podstawie
+wykona odpowiednie operacje i zwróci odpowiedni wynik
+lista_liczb = [10, 20, 30, 40]
+wynik = suma(lista_liczb)
+
+
+
+
+"""
+print("_"*65, "Zadanie 4")
+"""
+Zadanie 3.4 | Zliczanie znaków
+Napisz funkcję zliczającą liczbę znaków w podanym przez użytkownika napisie pomiędzy
+zadanymi przez użytkownika znakami (np. <>).
+Nawiasy mogą wystąpić tylko raz.
+Przykład użycia:
+policz_znaki('ala ma <kota> a kot ma ale')
+4
+policz_znaki('ala ma kota a (kot) ma ale', '(', ')')
+3
+"""
+print("_"*65, "Zadanie 5")
+print("_"*65, "Zadanie 6")
+print("_"*65, "Zadanie 7")
+print("_"*65, "Zadanie 8")
+print("_"*65, "Zadanie 9")
+print("_"*65, "Zadanie 10")
+print("_"*65, "Zadanie 11")
+print("_"*65, "Zadanie 12")
+
