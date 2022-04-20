bd96a8b46af1c3291ad349b2b5878d54cb23f13d
WojtekLewiński <lewinski.wojciech.dariusz@gmail.com>
2022-04-20 20:13:16 +0200

    Zadanie 10

--- Wojtek_Zadania.py
+++ Wojtek_Zadania.py
@@ -1211,6 +1211,6 @@ lista = ['Adrian','Piotrek','Ewa']
 for i in range(3):
     print(i, lista[i])
 
-for i in range(1, 11):
-    if (i % 8 != 0) and (i % 3 != 0) and (i % 9 != 0):
-        print(i)
+# for i in range(1, 11):
+#     if (i % 8 != 0) and (i % 3 != 0) and (i % 9 != 0):
+#         print(i)
